3037814c45f134d04af700a467461b173b07c602
Fabian Benschuh <Fabi.Benschuh@gmx.de>
2020-12-17 15:38:49 +0100

    fix formatting

--- gpx_to_png.py
+++ gpx_to_png.py
@@ -63,74 +63,80 @@ urls = {
     ]
 }
 
+
 def format_time(time_s):
     if not time_s:
         return 'n/a'
     minutes = math.floor(time_s / 60)
     hours = math.floor(minutes / 60)
-    return '%s:%s:%s' % (str(int(hours)).zfill(2), str(int(minutes % 60)).zfill(2), str(int(time_s % 60)).zfill(2)) 
+    return '%s:%s:%s' % (str(int(hours)).zfill(2), str(int(minutes % 60)).zfill(2), str(int(time_s % 60)).zfill(2))
 
-def osm_lat_lon_to_x_y_tile (lat_deg, lon_deg, zoom):
+
+def osm_lat_lon_to_x_y_tile(lat_deg, lon_deg, zoom):
     """ Gets tile containing given coordinate at given zoom level """
     # taken from http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames, works for OSM maps
     lat_rad = math.radians(lat_deg)
     n = 2 ** zoom
     xtile = int((lon_deg + 180) / 360 * n)
-    ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2 * n)
+    ytile = int((1.0 - math.log(math.tan(lat_rad) +
+                                (1 / math.cos(lat_rad))) / math.pi) / 2 * n)
     return (xtile, ytile)
 
-def osm_get_auto_zoom_level ( min_lat, max_lat, min_lon, max_lon, max_n_tiles):
+
+def osm_get_auto_zoom_level(min_lat, max_lat, min_lon, max_lon, max_n_tiles):
     """ Gets zoom level which contains at maximum `max_n_tiles` """
-    for z in range (0,17):
-        x1, y1 = osm_lat_lon_to_x_y_tile (min_lat, min_lon, z)
-        x2, y2 = osm_lat_lon_to_x_y_tile (max_lat, max_lon, z)
-        max_tiles = max (abs(x2 - x1), abs(y2 - y1))
+    for z in range(0, 17):
+        x1, y1 = osm_lat_lon_to_x_y_tile(min_lat, min_lon, z)
+        x2, y2 = osm_lat_lon_to_x_y_tile(max_lat, max_lon, z)
+        max_tiles = max(abs(x2 - x1), abs(y2 - y1))
         if (max_tiles > max_n_tiles):
-            print ("Max tiles: %d" % max_tiles)
-            return z 
+            print("Max tiles: %d" % max_tiles)
+            return z
     return 17
+
 
 class MapCacher:
     """ Class for caching maps """
 
-    def __init__(self, map:str, folder:str) -> None:
+    def __init__(self, map: str, folder: str) -> None:
         self.map_name = map
         self.servers = urls[map].copy()
         self.root = folder
 
-    def change_server(self, map:str) -> None:
+    def change_server(self, map: str) -> None:
         self.map_name = map
         self.servers = urls[map].copy()
 
-    def get_tile_urls (self, x:int, y:int, z:int):
-        remote = self.servers.copy()  
+    def get_tile_urls(self, x: int, y: int, z: int):
+        remote = self.servers.copy()
         for i in range(len(remote)):
-            remote[i] = remote[i].format(x = x, y = y, z = z)
+            remote[i] = remote[i].format(x=x, y=y, z=z)
         return remote
 
-    def get_tile_filename (self, x:int, y:int, z:int) -> str:
+    def get_tile_filename(self, x: int, y: int, z: int) -> str:
         return self.root + r"/%s/%d/%d/%d.png" % (self.map_name, z, x, y)
 
     def cache_area(self, x_min, x_max, y_min, y_max, z) -> None:
         """ Downloads necessary tiles to cache """
-        print ("Caching tiles x1=%d y1=%d x2=%d y2=%d" % (x_min, y_min, x_max, y_max))
-        for y in range (y_min, y_max + 1):
-            for x in range (x_min, x_max + 1):
-                self.cache_tile (x, y, z)
+        print("Caching tiles x1=%d y1=%d x2=%d y2=%d" %
+              (x_min, y_min, x_max, y_max))
+        for y in range(y_min, y_max + 1):
+            for x in range(x_min, x_max + 1):
+                self.cache_tile(x, y, z)
 
-    def cache_tile (self,x,y,z) -> None:
+    def cache_tile(self, x, y, z) -> None:
         """ Downloads tile x,y,x into cache. Directories are automatically created, existing files are not retrieved. """
-        src_urls = self.get_tile_urls(x,y,z)
-        dst_filename = self.get_tile_filename(x,y,z)
+        src_urls = self.get_tile_urls(x, y, z)
+        dst_filename = self.get_tile_filename(x, y, z)
 
         dst_dir = os.path.dirname(dst_filename)
         if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)    
-        if os.path.isfile (dst_filename):
+            os.makedirs(dst_dir)
+        if os.path.isfile(dst_filename):
             return
         data = None
         for i in range(len(src_urls)):
-            print ("Downloading from Mirror %d: %s ..." % (i, src_urls[i]))
+            print("Downloading from Mirror %d: %s ..." % (i, src_urls[i]))
             try:
                 response = requests.get(src_urls[i])
                 code = response.status_code
@@ -138,9 +144,9 @@ class MapCacher:
                     data = response.content
                     break
                 else:
-                    print ("Error occurred! Response code: %d" % code)
+                    print("Error occurred! Response code: %d" % code)
             except:
-                print ("ERROR BY ACCESSING URL: %s" % src_urls[i])
+                print("ERROR BY ACCESSING URL: %s" % src_urls[i])
         if data != None:
             f = open(dst_filename, "wb")
             f.write(data)
@@ -162,7 +168,7 @@ class MapCreator:
         self.h = (self.y2 - self.y1 + 1) * osm_tile_res
         self.z = z
         print(self.w, self.h)
-         
+
     def aspect_ratio(self, ratio_max, ratio_min):
         if (self.y2 - self.y1 + 1) / (self.x2 - self.x1 + 1) > ratio_max:
             self.x2 += 1
@@ -174,37 +180,39 @@ class MapCreator:
                 self.y1 -= 1
         self.w = (self.x2 - self.x1 + 1) * osm_tile_res
         self.h = (self.y2 - self.y1 + 1) * osm_tile_res
-        print (self.w, self.h)
+        print(self.w, self.h)
 
-    def create_area_background(self, map_cacher:MapCacher):
+    def create_area_background(self, map_cacher: MapCacher):
         """ Creates background map from cached tiles """
         map_cacher.cache_area(self.x1, self.x2, self.y1, self.y2, self.z)
-        self.dst_img = pil_image.new ("RGB", (self.w, self.h))      
-        for y in range (self.y1, self.y2+1):
-            for x in range (self.x1, self.x2+1):
+        self.dst_img = pil_image.new("RGB", (self.w, self.h))
+        for y in range(self.y1, self.y2+1):
+            for x in range(self.x1, self.x2+1):
                 try:
-                    src_img = pil_image.open (map_cacher.get_tile_filename (x, y, self.z))
+                    src_img = pil_image.open(
+                        map_cacher.get_tile_filename(x, y, self.z))
                 except Exception as e:
-                    print("Error processing file " + map_cacher.get_tile_filename (x, y, self.z))
+                    print("Error processing file " +
+                          map_cacher.get_tile_filename(x, y, self.z))
                     src_img = pil_image.open("error.png")
                 dst_x = (x-self.x1)*osm_tile_res
                 dst_y = (y-self.y1)*osm_tile_res
-                self.dst_img.paste (src_img, (dst_x, dst_y))
+                self.dst_img.paste(src_img, (dst_x, dst_y))
 
-    def lat_lon_to_image_xy (self, lat_deg, lon_deg):
+    def lat_lon_to_image_xy(self, lat_deg, lon_deg):
         """ Internal. Converts lat, lon into dst_img coordinates in pixels """
         lat_rad = math.radians(lat_deg)
         n = 2.0 ** self.z
         xtile_frac = (lon_deg + 180) / 360 * n
-        ytile_frac = (1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2 * n
-        img_x = int( (xtile_frac-self.x1)*osm_tile_res )
-        img_y = int( (ytile_frac-self.y1)*osm_tile_res )
+        ytile_frac = (1.0 - math.log(math.tan(lat_rad) +
+                                     (1 / math.cos(lat_rad))) / math.pi) / 2 * n
+        img_x = int((xtile_frac-self.x1)*osm_tile_res)
+        img_y = int((ytile_frac-self.y1)*osm_tile_res)
         return (img_x, img_y)
 
-
-    def draw_track (self, gpx, color, thickness):
+    def draw_track(self, gpx, color, thickness):
         """ Draw GPX track onto map """
-        draw = pil_draw.Draw (self.dst_img)
+        draw = pil_draw.Draw(self.dst_img)
         for track in gpx.tracks:
             for segment in track.segments:
                 idx = 0
@@ -212,17 +220,21 @@ class MapCreator:
                 y_from = 0
                 for point in segment.points:
                     if (idx == 0):
-                        x_from, y_from = self.lat_lon_to_image_xy (point.latitude, point.longitude)
+                        x_from, y_from = self.lat_lon_to_image_xy(
+                            point.latitude, point.longitude)
                     else:
-                        x_to, y_to = self.lat_lon_to_image_xy (point.latitude, point.longitude)
-                        draw.line ((x_from,y_from,x_to,y_to), color, thickness, "curve")
+                        x_to, y_to = self.lat_lon_to_image_xy(
+                            point.latitude, point.longitude)
+                        draw.line((x_from, y_from, x_to, y_to),
+                                  color, thickness, "curve")
                         x_from = x_to
                         y_from = y_to
                     idx += 1
 
     def save_image(self, filename):
-        print("Saving " + filename) 
-        self.dst_img.save (filename)
+        print("Saving " + filename)
+        self.dst_img.save(filename)
+
 
 if (__name__ == '__main__'):
     """ Program entry point """
@@ -230,9 +242,9 @@ if (__name__ == '__main__'):
     gpx_files = []
     if(len(sys.argv) > 1):
         for i in range(1, len(sys.argv)):
-            gpx_files.extend( glob.glob (r"{}/*.gpx".format(sys.argv[i])) )
+            gpx_files.extend(glob.glob(r"{}/*.gpx".format(sys.argv[i])))
     else:
-        gpx_files = glob.glob (r"*.gpx")
+        gpx_files = glob.glob(r"*.gpx")
 
     if not gpx_files:
         print('No GPX files given')
@@ -243,8 +255,9 @@ if (__name__ == '__main__'):
             gpx = gpxpy.parse(open(gpx_file))
 
             # Print some track stats
-            print ('--------------------------------------------------------------------------------')
-            print ('  GPX file     : %s' % gpx_file)
+            print(
+                '--------------------------------------------------------------------------------')
+            print('  GPX file     : %s' % gpx_file)
             start_time, end_time = gpx.get_time_bounds()
             print('  Started       : %s' % start_time)
             print('  Ended         : %s' % end_time)
@@ -252,26 +265,30 @@ if (__name__ == '__main__'):
             moving_time, stopped_time, moving_distance, stopped_distance, max_speed = gpx.get_moving_data()
             print('  Moving time   : %s' % format_time(moving_time))
             print('  Stopped time  : %s' % format_time(stopped_time))
-            print('  Max speed     : %2.2fm/s = %2.2fkm/h' % (max_speed, max_speed * 60 ** 2 / 1000))    
+            print('  Max speed     : %2.2fm/s = %2.2fkm/h' %
+                  (max_speed, max_speed * 60 ** 2 / 1000))
             uphill, downhill = gpx.get_uphill_downhill()
             print('  Total uphill  : %4.0fm' % uphill)
             print('  Total downhill: %4.0fm' % downhill)
             min_lat, max_lat, min_lon, max_lon = gpx.get_bounds()
-            print("  Bounds        : [%1.4f,%1.4f,%1.4f,%1.4f]" % (min_lat, max_lat, min_lon, max_lon))
-            z = osm_get_auto_zoom_level (min_lat, max_lat, min_lon, max_lon, max_tile)
+            print("  Bounds        : [%1.4f,%1.4f,%1.4f,%1.4f]" % (
+                min_lat, max_lat, min_lon, max_lon))
+            z = osm_get_auto_zoom_level(
+                min_lat, max_lat, min_lon, max_lon, max_tile)
             print("  Zoom Level    : %d" % z)
 
             # Cache the map
             map_cacher = MapCacher("terrain", "tmp")
 
             # Create the map
-            map_creator = MapCreator (min_lat-margin, max_lat+margin, min_lon-margin, max_lon+margin, z)
+            map_creator = MapCreator(
+                min_lat-margin, max_lat+margin, min_lon-margin, max_lon+margin, z)
             map_creator.aspect_ratio(2, 1.5)
             map_creator.create_area_background(map_cacher)
-            map_creator.draw_track(gpx, (255,0,0), 4)
-            map_creator.save_image (gpx_file[:-4] + '-map.png')
+            map_creator.draw_track(gpx, (255, 0, 0), 4)
+            map_creator.save_image(gpx_file[:-4] + '-map.png')
 
         except Exception as e:
             logging.exception(e)
             print('Error processing %s' % gpx_file)
-            sys.exit(1) 
+            sys.exit(1)
